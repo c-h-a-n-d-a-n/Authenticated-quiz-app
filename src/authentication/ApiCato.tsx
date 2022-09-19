a200883d54d4dd9ce087abd89705db32282724a6
c-h-a-n-d-a-n <chandankumar773969@gmail.com>
2022-09-19 12:55:56 +0530

    first

---
+++ src/authentication/ApiCato.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ApiCato() {
+    
+  return (
+    <div>ApiCato</div>
+  )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+           <div className='innerFooter'>
+                Developed by <a target='blank' href="https://github.com/c-h-a-n-d-a-n">Chandan Kumar</a> | <a href="https://github.com/c-h-a-n-d-a-n/Authenticated-quiz-app.git" target='blank'>GITHUB REPO</a>   
+            </div> 
+        </div>
+    )
+}
+export default Footer
